c99cd0c7a9a180c768de41700b089a35a119d982
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-05-04 16:39:01 +0100

    Added TextQuery function

--- src/components/Chatbot/Chatbot.js
+++ src/components/Chatbot/Chatbot.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import "../../styles/Chatbot.css";
 import timeChange from "../../helpers/timeChange";
 
@@ -9,12 +10,52 @@ const boarderStyles = {
 };
 
 function Chatbot() {
+  const textQuery = async (text) => {
+    let conversation = {
+      who: "user",
+      content: {
+        text: {
+          text,
+        },
+      },
+    };
+
+    const textQueryVariables = {
+      text,
+    };
+
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/api/df_text_query",
+        textQueryVariables
+      );
+      const content = response.data.fulfillmentMessages[0];
+
+      conversation = {
+        who: "bot",
+        content,
+      };
+
+      console.log(conversation);
+    } catch (error) {
+      conversation = {
+        who: "bot",
+        content: {
+          text: {
+            text: "An error has occurred",
+          },
+        },
+      };
+    }
+  };
   // eslint-disable-next-line consistent-return
   const keyPressHandler = (event) => {
     if (event.key === "Enter") {
       if (!event.target.value) {
         return alert("You need to type a message");
       }
+
+      textQuery(event.target.value);
       // eslint-disable-next-line no-param-reassign
       event.target.value = "";
     }
@@ -25,7 +66,7 @@ function Chatbot() {
       <input
         className={`message-field ${timeChange(boarderStyles)}`}
         placeholder="Type your message here.. "
-        onKetPress={keyPressHandler}
+        onKeyPress={keyPressHandler}
         type="text"
       />
     </div>
